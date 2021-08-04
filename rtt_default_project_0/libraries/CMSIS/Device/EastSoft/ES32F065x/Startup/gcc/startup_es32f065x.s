dde425af30e8a75cf601fcf85eb33c3a859c4d58
liuhy <liuhongyan98@163.com>
2021-08-04 11:21:00 +0800

    update drivers.

--- rtt_default_project_0/libraries/CMSIS/Device/EastSoft/ES32F065x/Startup/gcc/startup_es32f065x.s
+++ rtt_default_project_0/libraries/CMSIS/Device/EastSoft/ES32F065x/Startup/gcc/startup_es32f065x.s
@@ -11,7 +11,27 @@
   *                  calls main()).
   *            After Reset the Cortex-M0 processor is in Thread mode,
   *            priority is Privileged, and the Stack is set to Main.
-  ******************************************************************************
+  * @note
+  *          Change Logs:
+  *          Date            Author          Notes
+  *          10 Dec 2018     AE Team         The first version
+  *
+  * Copyright (C) Shanghai Eastsoft Microelectronics Co. Ltd. All rights reserved.
+  *
+  * SPDX-License-Identifier: Apache-2.0
+  *
+  * Licensed under the Apache License, Version 2.0 (the License); you may
+  * not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  **********************************************************************************
   */
 
   .syntax unified
@@ -73,9 +93,9 @@ LoopFillZerobss:
   cmp r2, r4
   bcc FillZerobss
 
-/*bl __libc_init_array
+/*  bl __libc_init_array
 
-  bl main */
+  bl main*/
   bl entry
 
 LoopForever:
